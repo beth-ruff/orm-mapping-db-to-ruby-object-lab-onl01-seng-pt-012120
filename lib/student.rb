2f9a5a4d781156a32474c90fae26411e87eda819
Beth Ruff <beth.ruff2@gmail.com>
2020-03-25 01:30:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,9 +78,10 @@ class Student
     LIMIT 1
     SQL
     
-    DB[:conn].execute(sql)[0]
+    DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
-  end 
+  end.first 
+end
   
   def save
     sql = <<-SQL
