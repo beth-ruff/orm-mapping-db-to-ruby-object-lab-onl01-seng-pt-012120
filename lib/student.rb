23657d316d5401d335ab16f1f17e5e4f01a5cce8
Beth Ruff <beth.ruff2@gmail.com>
2020-03-25 01:10:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,9 +15,9 @@ class Student
     FROM students
     SQL
     
-    DB[:conn].execute(sql).map do |row|
-    end 
-    self.new_from_db(row)
+    DB[:conn].execute(sql)
+    # end 
+    # self.new_from_db(row)
   end
 
   def self.find_by_name(name)
